9784347380230ed9fda028b6328798b3117029b9
ajaxhe-bbs <ajaxhe@163.com>
2013-11-22 17:09:14 +0800

    Change app name with .out suffix

--- blobtrack_sample/build.sh
+++ blobtrack_sample/build.sh
@@ -2,5 +2,5 @@
 
 for i in *.cpp
 do
-	g++ -g `pkg-config --cflags opencv` -o `basename $i .cpp` $i `pkg-config --libs opencv`;
+	g++ -g `pkg-config --cflags opencv` -o `basename $i .cpp`.out $i `pkg-config --libs opencv`;
 done
